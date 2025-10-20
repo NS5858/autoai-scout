320f46e6b3f2362a40ab2f7cf74d1b0ef4793adb
NS5858 <enes.koese86@gmail.com>
2025-10-20 13:56:25 +0200

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 playwright install chromium
-uvicorn backend.main:app --host 0.0.0.0 --port 10000
-
+uvicorn main:app --host 0.0.0.0 --port 10000
